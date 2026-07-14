7943d251a6fc119e29fda50d63473b1e2a0146b4
jajhehbreal <narutokokoku@gmail.com>
2026-07-14 21:59:29 +0600

    Add licensing information to main.py
    Added licensing information and copyright notice to main.py.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-# very importent thing are in ALL CAPS
-# THIS WILL TAKE MATHS LIKE 1 + 1 AND TURN THEM INTO ONE PLUS ONE AKA TURN IT INTO ENGLISH TEXT SO JUST OUT PUT TOKENS
+# This file is part of O-positive-test-with-python.
+# Copyright (C) 2026 jajhehbreal
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from run_lang import run_langu
 
 class Main:
