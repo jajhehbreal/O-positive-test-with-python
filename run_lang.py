4b43351c28d1eeac27d9c20a21e20e0ffd72530c
jajhehb <narutokokoku@gmail.com>
2026-07-28 12:14:29 +0600

    Refactor run_lang.py with new lexer and parser
    Updated lexer and parser imports, added token array handling.

--- run_lang.py
+++ run_lang.py
@@ -14,22 +14,31 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
 from sys import exit
-from front_end.LEXER import Lexer
-from front_end.Parser import Pratt_Parser
+from front_end.Lexer import Lexer
+from front_end.Parser import Top_level_parser
 from numpy import fromiter,dtype
-from sys import stdout
 
 TOKEN_DTYPE = dtype([('type', 'U32'), ('value', 'O')])
 
 def run_langu(user_input):
     # 1. Lexer produces a generator
-    lexer = Lexer(user_input)
+    lexer_instance = Lexer(user_input)
+    # Stream directly into NumPy.
+    tokens_array = fromiter(lexer_instance.tokenize(), dtype=TOKEN_DTYPE)
+
+    if len(tokens_array) == 1 and tokens_array[0]['type'] == 'IDENTIFIER' and tokens_array[0]['value'] == 'exit':
+        print('exiting...\nDone')
+        exit()
+
+    #print
+    print(tokens_array)
     
     # 2. (Optional) convert to NumPy array for density, or just pass the generator
     #    For now, pass the generator directly — it will be converted to list inside the parser.
-    parser = Pratt_Parser(lexer.tokenize())
+    #parser = Top_level_parser(lexer.tokenize())
     
     # 3. Parse and get the AST
-    ast = parser.parse()
-    return ast
+    #ast = parser.parse_program()
+    #return ast
